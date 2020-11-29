52c85f3a6399d24dca8dd8a32525b48639433388
hamzakhalid99 <53593638+hamzakhalid99@users.noreply.github.com>
2020-11-29 21:38:05 +0500

    Updated the tables

--- Customer/Water_Page.php
+++ Customer/Water_Page.php
@@ -89,20 +89,8 @@
               <th class="font-weight-light"><?php echo $_SESSION['name'] ?></th>
             </tr>
              <tr>
-              <th class="font-weight-light">EMAIL</th>
-              <th class="font-weight-light"><?php echo $_SESSION['email_id'] ?></th>
-            </tr>
-             <tr>
-              <th class="font-weight-light">CNIC</th>
-              <th class="font-weight-light"><?php echo $_SESSION['cnic'] ?></th>
-            </tr>
-             <tr>
               <th class="font-weight-light">ADDRESS</th>
               <th class="font-weight-light"><?php echo $_SESSION['address'] ?></th>
-            </tr>
-             <tr>
-              <th class="font-weight-light">CONTACT NO</th>
-              <th class="font-weight-light"><?php echo $_SESSION['contact_number'] ?></th>
             </tr>
              </tr>
              <tr>
@@ -110,7 +98,8 @@
               <th class="font-weight-light">Due Date (we will insert this after billing is finalised)</th>
             </tr>
           </table>
-      
+          
+        <h2 style="font-size: 15px;text-align:right;">In case of any discrepancies, please contact us at 0900-78601</h2>
 
         </div>
       </div>
