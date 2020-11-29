f101c7ef6d64fba7760c57e04083fdbb525d9f5d
mursal16 <mursaljunaidrehman@hotmail.com>
2020-11-29 16:16:23 +0500

    working version for updating utility

--- Admin/update.php
+++ Admin/update.php
@@ -1,13 +1,3 @@
-
-<?php
-
-include "../DB_Connection/database_connection.php";
-
-$id = $_GET['id']; // get id through query string
-$update_q = mysqli_query($connect,"select * from Utilities where utility_id='$id'"); // select query
-$data = mysqli_fetch_array($update_q); // fetch data
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,24 +6,29 @@ $data = mysqli_fetch_array($update_q); // fetch data
 <h3>Update Data</h3>
 
 <form method="POST">
-  Monthly Price<input type="text" name="fixed_monthly_price" value="<?php echo $data['fixed_monthly_price'] ?>" placeholder=" Enter fixed monthly price" Required>
-  Unit Price<input type="text" name="unit_price" value="<?php echo $data['unit_price'] ?>" placeholder=" Enter unit price" Required>
-  <input type="submit" name="update" value="Update">
+  <input type="text" name="fixed_monthly_price" placeholder=" Enter fixed monthly price" Required><br/>
+  <input type="text" name="unit_price" placeholder=" Enter unit price" Required><br/>
+  <input type="submit" name="update" value="update">
 </form>
 <?php
+include "../DB_Connection/database_connection.php";
+
+
+//$update_q = mysqli_query($connect,"select * from Utilities where utility_id='$id'"); // select query
+//$data = mysqli_fetch_array($update_q); // fetch data
 if(isset($_POST['update'])) // when click on Update button
 {
     echo "Changes saved";
-
-    $monthly = $_GET['fixed_monthly_price'];
-    $unit = $_GET['unit_price'];
-	
-    $edit = mysqli_query($connect,"update Utilities set fixed_monthly_price='$monthly', unit_price='$unit' where id='$id'");
+    $id = $_GET['id']; // get id through query string
+    echo $id;
+    $monthly = $_POST['fixed_monthly_price'];
+    $unit = $_POST['unit_price'];
+    $edit = mysqli_query($connect,"update Utilities set fixed_monthly_price='$monthly', unit_price='$unit' where utility_id='$id'");
 	
     if($edit)
     {
         mysqli_close($connect);
-        header("location:all_records.php"); // redirects to all records page
+        header("location:view_update.php"); // redirects to all records page
         exit;
     }
     else
